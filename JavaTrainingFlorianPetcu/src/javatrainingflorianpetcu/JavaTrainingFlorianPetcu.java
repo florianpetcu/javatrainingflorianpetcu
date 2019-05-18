a9677ad5c6af50eee6c4f9eeaf77a881d72e2c79
asus <asus@DESKTOP-VTC14O7>
2019-05-18 10:49:57 +0300

    Am facut tema pentru versionare si va fi primul meu cod scris in Java

---
+++ JavaTrainingFlorianPetcu/src/javatrainingflorianpetcu/JavaTrainingFlorianPetcu.java
@@ -0,0 +1,28 @@
+/*
+* this id my first versioned program 
+*/
+package javatrainingflorianpetcu;
+
+/**
+ * this id my first versioned program
+ * @author asus
+ */
+public class JavaTrainingFlorianPetcu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("this is my first program in Java");
+        System.out.println("this is a new line first program in Java");
+        System.out.println("this is my very first program in Java");
+        System.out.println("this is not my first program in Java");
+        System.out.println("this is the only first program in Java");
+        int lungimeListaParamtrii = args.length;
+        for(int contor=0;contor<lungimeListaParamtrii;contor++){
+            System.out.println("parameter number "+contor+" is "+args[contor]);
+        }
+    }
+    
+    
+}
